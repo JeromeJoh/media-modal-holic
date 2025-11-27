436296bfc49b58261656077d3697678e1aa52c79
JeromeJoh <jerome.yzhao@foxmail.com>
2025-11-27 11:05:43 +0800

    feat: add & test AudioModal

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import VideoModal from './component/media-modal-holic/video-modal/index.js';
 import ImageModal from './component/media-modal-holic/image-modal/index.js';
+import AudioModal from './component/media-modal-holic/audio-modal/index.js';
 
 customElements.define('video-modal-holic', VideoModal);
 customElements.define('image-modal-holic', ImageModal);
+customElements.define('audio-modal-holic', AudioModal);
