823247d6c450433d04846a2bbcbf136d0eb6e67c
JeromeJoh <jerome.yzhao@foxmail.com>
2025-12-08 23:55:11 +0800

    refactor: use BaseComponent class

--- component/media-modal-holic/base-modal/index.js
+++ component/media-modal-holic/base-modal/index.js
@@ -1,22 +1,110 @@
-export default class MediaModal extends HTMLElement {
-  static get observedAttributes() { return ['type', 'src']; }
+import { loadExternalResource } from '../utils/index.js';
+
+const STRUCTURE_HOOKS = ["_preRender", "_render", "_cacheElements"];
+const BEHAVIOR_HOOKS = ["_bindEvents", "_afterInit"];
+
+class BaseComponent extends HTMLElement {
+  #ready = null;
+  #resolveReady = null;
+
+  constructor() {
+    super();
+    this.#ready = new Promise(res => (this.#resolveReady = res));
+  }
+
+  connectedCallback() {
+    this.#run();
+  }
+
+  async #run() {
+    // await this.#runHook("_preRender");
+    // await this.#runHook("_render");
+    // await this.#runHook("_cacheElements");
+    // await this.#runHook("_bindEvents");
+    // await this.#runHook("_afterInit");
+    await this.runHooks(this);
+
+    this.#resolveReady();
+  }
+
+  async #runHook(name) {
+    const fns = this.#collectHookFns(name);
+    console.log('BASE COMPONENT #runHook', name, fns);
+    for (const fn of fns) {
+      const ret = fn.call(this);
+      console.log('BASE COMPONENT ret', ret);
+      if (ret instanceof Promise) await ret;
+    }
+  }
+
+  async runHooks(component) {
+    function getPrototypeChain(instance) {
+      const chain = [];
+      let proto = Object.getPrototypeOf(instance);
+
+      while (proto && proto !== HTMLElement.prototype) {
+        chain.push(proto);
+        proto = Object.getPrototypeOf(proto);
+      }
+
+      // 从最顶层 Base → 子类顺序
+      return chain.reverse();
+    }
+
+    const protos = getPrototypeChain(component); // Base → Parent → Child
+
+    // 结构阶段
+    for (const proto of protos) {
+      for (const hook of STRUCTURE_HOOKS) {
+        const result = proto[hook]?.call(this);
+        if (result instanceof Promise) {
+          await result; // 等待 async hook 完成
+        }
+      }
+    }
+
+    // 行为阶段
+    for (const proto of protos) {
+      for (const hook of BEHAVIOR_HOOKS) {
+        proto[hook]?.call(component);
+      }
+    }
+  }
+
+  // 按继承链顺序收集所有 hook（父→子）
+  #collectHookFns(name) {
+    const fns = [];
+    let proto = Object.getPrototypeOf(this);
+
+    while (proto && proto !== HTMLElement.prototype) {
+      const fn = proto[name];
+      if (typeof fn === "function") fns.unshift(fn);
+      proto = Object.getPrototypeOf(proto);
+    }
+    return fns;
+  }
+
+  ready() {
+    return this.#ready;
+  }
+}
+
+
+export default class MediaModal extends BaseComponent {
+  static get observedAttributes() { return ['src']; }
 
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    this.showControls = this.hasAttribute('controls');
+    console.log('BASE MODAL constructor');
   }
 
   async connectedCallback() {
-    await this._preRender();
-    this._render();
-    this._cacheElements();
-    this._bindEvents();
-    this._afterInit();
+    super.connectedCallback?.();
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (oldValue !== newValue) this._updateContent();
+    if (oldValue !== newValue) return;
   }
 
   async _loadTemplate() {
@@ -54,20 +142,62 @@ export default class MediaModal extends HTMLElement {
   }
 
   async _preRender() {
-    await this._loadTemplate();
+    console.log('BASE MODAL _preRender');
+    this.src = this.getAttribute('src');
+    this.showControls = this.hasAttribute('controls');
+    this.setAttribute('tabindex', '0');
   }
 
   async _render() {
-    this._updateContent();
-    const baseCSS = await fetch(new URL('./style.css', import.meta.url)).then(res => res.text());
-    const baseSheet = new CSSStyleSheet();
-    baseSheet.replaceSync(baseCSS);
+    console.log('BASE MODAL _render');
+    const [html, css] = await Promise.all([loadExternalResource('./template.html', import.meta.url), loadExternalResource('./style.css', import.meta.url)]);
 
-    // 合并到 adoptedStyleSheets
+    this.shadowRoot.innerHTML = html;
+
+    const baseSheet = new CSSStyleSheet();
+    baseSheet.replaceSync(css);
     this.shadowRoot.adoptedStyleSheets = [baseSheet];
+
+    console.log('BASE MODAL _render done', this.shadowRoot.querySelector('.modal'));
   }
-  _bindEvents() { }
-  _cacheElements() { }
-  _afterInit() { }
+
+  _bindEvents() {
+    console.log('BASE MODAL _bindEvents');
+    this.addEventListener('keydown', (e) => {
+      console.log('BASE MODAL open()', this, document.activeElement);
+      // 只在聚焦时响应
+      if (e.key === ' ' || e.key === 'Enter') {
+        e.preventDefault();
+        this.open();   // 自己的打开模态框方法
+      }
+
+      if (e.key === 'Escape') {
+        e.preventDefault();
+        this.close();  // 自己的关闭模态框方法
+      }
+    });
+
+
+    this.$thumb.addEventListener('click', this.open.bind(this));
+
+    this.$modal.addEventListener('click', this.close.bind(this));
+  }
+
+  open() {
+    this.$modal.classList.add('active');
+    console.log('BASE MODAL open()');
+  }
+  close() {
+    this.$modal.classList.remove('active');
+    console.log('BASE MODAL close()');
+  }
+  _cacheElements() {
+    this.$modal = this.shadowRoot.querySelector('.modal');
+    this.$thumb = this.shadowRoot.querySelector('.thumb');
+    console.log('BASE MODAL _cacheElements', this.shadowRoot, this.$modal, this.$thumb);
+  }
+  _afterInit() {
+    console.log('BASE MODAL _afterInit', this.$modal, this.$thumb);
+  }
   _cleanup() { }
 }
--- component/media-modal-holic/video-modal/index.js
+++ component/media-modal-holic/video-modal/index.js
@@ -1,87 +1,25 @@
 import MediaModal from '../base-modal/index.js';
-
-console.log('VideoModal loaded', MediaModal);
+import { loadExternalResource } from '../utils/index.js';
 
 export default class VideoModal extends MediaModal {
   constructor() {
-    super()
-    console.log('VideoModal constructor', this.shadowRoot.innerHTML);
+    super();
+    console.log('VIDEO MODAL constructor');
   }
 
   async connectedCallback() {
-    await super.connectedCallback();
-    this.thumbVideo = this.shadowRoot.querySelector('.thumb video');
-    this.modal = this.shadowRoot.querySelector('.modal');
-    console.log('modal', this.shadowRoot, this.modal, this.thumbVideo);
-    this.modalVideo = this.modal.querySelector('video');
-
-    // hover preview (loop first 5 seconds)
-    this.thumbVideo.addEventListener("mouseenter", () => {
-      this.thumbVideo.currentTime = 0;
-      this.thumbVideo.play();
-      this.previewTimer = setInterval(() => {
-        if (this.thumbVideo.currentTime >= 5) {
-          this.thumbVideo.currentTime = 0;
-        }
-      }, 200);
-    });
-
-    this.thumbVideo.addEventListener("mouseleave", () => {
-      clearInterval(this.previewTimer);
-      this.thumbVideo.pause();
-      this.thumbVideo.currentTime = 0;
-    });
-
-    // click → open modal
-    this.shadowRoot.querySelector('.thumb').addEventListener('click', () => {
-      this.modal.classList.add('active');
-
-
-      // video 弹出动画
-      const modalAnim = this.modalVideo.animate(
-        [
-          { opacity: 0, transform: "scale(0)" },
-          { opacity: 1, transform: "scale(1)" }
-        ],
-        {
-          duration: 300,
-          easing: "cubic-bezier(0.22, 1, 0.36, 1)"
-        }
-      );
-
-      modalAnim.onfinish = () => {
-        this.modalVideo.currentTime = 0;
-        this.modalVideo.play();
-      }
-
-      // 背景淡入
-      this.modal.animate(
-        [
-          { opacity: 0 },
-          { opacity: 1 }
-        ],
-        {
-          duration: 250,
-          easing: "ease-out"
-        }
-      );
-    });
-
-    // click outside video → close
-    this.modal.addEventListener('click', (e) => {
-      if (e.target === this.modal) {
-        this.close();
-      }
-    });
+    await super.connectedCallback?.();
+    console.log('VIDEO ====== modal', this.$modal);
   }
 
   close() {
-    const bg = this.modal.animate(
+    super.close?.();
+    this.$modal.animate(
       [{ opacity: 1 }, { opacity: 0 }],
       { duration: 200, easing: "ease-in" }
     );
 
-    const videoAnim = this.modalVideo.animate(
+    const videoAnim = this.$modalVideo.animate(
       [
         { opacity: 1, transform: "scale(1)" },
         { opacity: 0, transform: "scale(0.85)" }
@@ -90,8 +28,90 @@ export default class VideoModal extends MediaModal {
     );
 
     videoAnim.onfinish = () => {
-      this.modal.classList.remove('active');
-      this.modalVideo.pause();
+      this.$modalVideo.pause();
     };
+  }
+
+  open() {
+    super.open?.();
+
+    console.log('OPEN MODAL AT VIDEO OPEN');
+    // video 弹出动画
+    const modalAnim = this.$modalVideo.animate(
+      [
+        { opacity: 0, transform: "scale(0)" },
+        { opacity: 1, transform: "scale(1)" }
+      ],
+      {
+        duration: 300,
+        easing: "cubic-bezier(0.22, 1, 0.36, 1)"
+      }
+    );
+
+    modalAnim.onfinish = () => {
+      console.log(23345345, this.$modalVideo);
+      this.$modalVideo.currentTime = 0;
+      this.$modalVideo.play();
+    }
+
+    // 背景淡入
+    this.$modal.animate(
+      [
+        { opacity: 0 },
+        { opacity: 1 }
+      ],
+      {
+        duration: 250,
+        easing: "ease-out"
+      }
+    );
+  }
+
+  async _render() {
+    console.log('VIDEO MODAL _render', this.$modal, this.$thumb);
+    const [html, css] = await Promise.all([loadExternalResource('./template.html', import.meta.url), loadExternalResource('./style.css', import.meta.url)]);
+
+    this.$thumb.innerHTML = this.$modal.innerHTML = `<video src="${this.src}" ${this.showControls ? 'controls' : ''} muted></video>`;
+
+    const baseSheet = new CSSStyleSheet();
+    baseSheet.replaceSync(css);
+
+    this.shadowRoot.adoptedStyleSheets = [
+      ...this.shadowRoot.adoptedStyleSheets,
+      baseSheet
+    ];
+  }
+
+  _preRender() {
+    console.log('VIDEO MODAL _preRender');
+  }
+
+  _bindEvents() {
+    // hover preview (loop first 5 seconds)
+    this.$thumbVideo.addEventListener("mouseenter", () => {
+      this.$thumbVideo.currentTime = 0;
+      this.$thumbVideo.play();
+      this.previewTimer = setInterval(() => {
+        if (this.$thumbVideo.currentTime >= 5) {
+          this.$thumbVideo.currentTime = 0;
+        }
+      }, 200);
+    });
+
+    this.$thumbVideo.addEventListener("mouseleave", () => {
+      clearInterval(this.previewTimer);
+      this.$thumbVideo.pause();
+      this.$thumbVideo.currentTime = 0;
+    });
+  }
+  _cacheElements() {
+    console.log('VIDEO MODAL _cacheElements', this.$modal, this.$thumb);
+    this.$thumbVideo = this.$thumb.querySelector('video');
+    this.$modalVideo = this.$modal.querySelector('video');
+    console.log('VIDEO MODAL _cacheElements', this.$modalVideo, this.$thumbVideo);
+  }
+  _afterInit() {
+  }
+  _cleanup() {
   }
 }
