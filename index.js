243e042d302bf70db76696c08184466c2feeaeea
JeromeJoh <jerome.yzhao@foxmail.com>
2025-12-14 23:50:07 +0800

    chore: add todos

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ import AudioModal from './component/media-modal-holic/audio-modal/index.js';
 customElements.define('video-modal-holic', VideoModal);
 customElements.define('image-modal-holic', ImageModal);
 customElements.define('audio-modal-holic', AudioModal);
+
+
+// TODO: add modal open animation - V M A letter animation
+// TODO: seperate dom structure to html files
